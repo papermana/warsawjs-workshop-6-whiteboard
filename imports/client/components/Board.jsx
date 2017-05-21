f9956a4d237ebb4d527aeb73dfab54630d58c666
Dominik Rowicki <dominik.rowicki@netguru.co>
2017-05-21 14:06:50 +0200

    Create a FabricObjects collection and add methods for canvas to update the DB

--- imports/client/components/Board.jsx
+++ imports/client/components/Board.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import {fabric} from 'fabric';
+import FabricObjects from '../../lib/fabric-objects';
 
 export default class Board extends React.Component {
+  drawnObjects = new Map();
+
   componentDidMount = () => {
     this.canvas = new fabric.Canvas(this.refs.canvas, {
       isDrawingMode: this.props.isDrawingAllowed,
+    });
+
+    this.canvas.on('object:added', async ({target}) => {
+      const id = await FabricObjects.genInsert(target);
+
+      this.drawnObjects.set(target, id);
+    });
+
+    this.canvas.on('object:modified', async ({target}) => {
+      const id = await this.drawnObjects.get(target);
+
+      FabricObjects.update({_id: id}, target);
+    });
+
+    this.canvas.on('object:removed', async ({target}) => {
+      const id = await this.drawnObjects.get(target);
+
+      FabricObjects.remove({_id: id});
     });
   }
 
--- server/main.js
+++ server/main.js
@@ -1,1 +1,2 @@
 import { Meteor } from 'meteor/meteor';
+import '../imports/lib/fabric-objects';
