43604ab7ad4de31b641193bc634b97cec54f702a
Dominik Rowicki <dominik.rowicki@netguru.co>
2017-05-21 15:01:30 +0200

    Handle adding, updating and removing canvas objects from DB

--- imports/client/components/Board.jsx
+++ imports/client/components/Board.jsx
@@ -17,21 +17,44 @@ export default class Board extends React.Component {
     });
 
     this.canvas.on('object:added', async ({target}) => {
-      const id = await FabricObjects.genInsert(target);
+      try {
+        const id = await FabricObjects.genInsert(target.toObject());
 
-      this.drawnObjects.set(target, id);
+        this.drawnObjects.set(target, id);
+      } catch (e) {
+        console.error(
+          'Something went wrong when inserting a canvas object in DB: ',
+          e
+        );
+      }
     });
 
     this.canvas.on('object:modified', async ({target}) => {
-      const id = await this.drawnObjects.get(target);
+      try {
+        const id = this.drawnObjects.get(target);
 
-      FabricObjects.update({_id: id}, target);
+        FabricObjects.genUpdate({_id: id}, target.toObject());
+      } catch (e) {
+        console.error(
+          'Something went wrong when updating a canvas object in DB: ',
+          e
+        );
+      }
     });
 
     this.canvas.on('object:removed', async ({target}) => {
-      const id = await this.drawnObjects.get(target);
+      try {
+        const id = this.drawnObjects.get(target);
 
-      FabricObjects.remove({_id: id});
+        FabricObjects.genRemove({_id: id});
+      } catch (e) {
+        console.error(
+          'Something went wrong when removing a canvas object from DB: ',
+          e
+        );
+      }
+    });
+
     });
   }
 
--- imports/lib/fabric-objects.js
+++ imports/lib/fabric-objects.js
@@ -12,6 +12,30 @@ class Collection extends Mongo.Collection {
       });
     });
   }
+
+  async genUpdate(query, doc) {
+    return new Promise((resolve, reject) => {
+      this.update(query, doc, (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+
+  async genRemove(query) {
+    return new Promise((resolve, reject) => {
+      this.remove(query, (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
 }
 
 export default new Collection('fabricObjects');
